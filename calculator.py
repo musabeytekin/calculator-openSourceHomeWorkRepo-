17c27036de87d85c23ed381cf6297fe2b1da20af
musabeytekin <02190201003@ogr.inonu.edu.tr>
2022-12-16 17:40:25 +0300

    inital

---
+++ calculator.py
@@ -0,0 +1,29 @@
+def add(x, y):
+   """This function adds two numbers"""
+
+   return x + y
+
+def subtract(x, y):
+   """This function subtracts two numbers"""
+
+   return x - y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+
+
+choice = input("Enter choice(1,2):")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+
+else:
+   print("Invalid input")
